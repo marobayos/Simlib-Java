c5d1b960add835cd3beb75b3680db23b072cd74f
jedcastroag <jedcastroag@unal.edu.co>
2018-03-17 19:39:12 -0500

    Ahora los Eventos pueden tener cualquier cantidad de atributos de tipo float

--- src/simlib/Event.java
+++ src/simlib/Event.java
@@ -3,24 +3,23 @@ package simlib;
 public class Event implements Comparable<Event>{
     private byte eventType, origen;
     private float time;
+    private float atributes[];
 
-    public Event(byte type, float time) {
+    public Event(byte type, float time, float... atributes) {
         this.eventType = type;
         this.time = time;
-        this.origen = 2;
-    }
-    public Event(byte type, float time, byte origen) {
-        this.eventType = type;
-        this.time = time;
-        this.origen = origen;
+        this.atributes = atributes;
     }
 
     public byte getType() {
         return this.eventType;
     }
 
-    public byte getOrigen() {
-        return origen;
+    public float getAtribute(int atribute){
+        if(atributes.length != 0){
+            return atributes[atribute];
+        }
+        return (float) -10E30;
     }
 
     public float getTime(){
