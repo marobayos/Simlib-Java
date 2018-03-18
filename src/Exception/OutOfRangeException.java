b101da549df3e82d57a59c816b8466db52705872
iHollyDay <marobayos@unal.edu.co>
2018-03-17 19:55:12 -0500

    El

--- src/Exception/OutOfRangeException.java
+++ src/Exception/OutOfRangeException.java
@@ -2,6 +2,6 @@ package Exception;
 
 public class OutOfRangeException extends RuntimeException {
 	public OutOfRangeException(String name, int size, int index) {
-		super("The " + name + " has not ");
+		super("The " + name + " has not "+index+" beause has only "+size);
 	}
 }
