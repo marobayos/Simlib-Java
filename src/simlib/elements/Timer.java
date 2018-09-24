6859e3b055cab7714c903f256cb73fe032f6e4b5
María Alejandra Robayo Sepúlveda <marobayos@unal.edu.co>
2018-09-24 07:37:21 -0500

    Update Timer.java

--- src/simlib/elements/Timer.java
+++ src/simlib/elements/Timer.java
@@ -2,9 +2,17 @@ package simlib.elements;
 
 public class Timer {
 	private float present;
+	private static final Timer timer;
 
 	public Timer() {
 		present = 0;
+	}
+	
+	public static Timer getTimer(){
+		if( timer == null){
+			timer = new Timer();
+		}
+		return timer;
 	}
 
 	public void setTime(float time) {
